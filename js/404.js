44f0d523b4bddcafde0b6fe1f8b9d70dfd2d8329
Sumit Soni <sonisumit392@gmail.com>
2024-01-11 01:28:49 +0530

    page completed

--- js/404.js
+++ js/404.js
@@ -1,3 +1,6 @@
 // Parallax Code
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
+document.body.addEventListener('click', function() {
+    window.location.href = '/'; // Replace with your default URL if it's not the root
+});
