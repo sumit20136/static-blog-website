2564f86dab80e44c115d1e7d4c8719daf8ddfa71
Sumit Soni <sonisumit392@gmail.com>
2024-01-11 01:33:21 +0530

    404 done

---
+++ js/404.js
@@ -0,0 +1,7 @@
+// Parallax Code
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+document.body.addEventListener('click', function() {
+    window.location.href = 'https://sumit20136.github.io/static-blog-website/'; 
+});
